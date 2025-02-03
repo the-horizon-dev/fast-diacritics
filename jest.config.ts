b80c363542b52928df71721632357849b10391e8
Maximiliano Veiga <maxsantosneto@gmail.com>
2025-02-03 17:35:06 -0300

    Update Jest configuration for ESM support and enhance module exports in package.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,22 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageThreshold: {
